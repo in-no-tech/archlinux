17fbda80498935ddc058debcb87dfea6360bbb47
in-no-tech <82801408+in-no-tech@users.noreply.github.com>
2021-04-19 12:20:37 +0400

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+
+sgdisk --zap-all /dev/sda
+sgdisk -o /dev/sda
+sgdisk -n 1:0:+256M -t 1:ef00 -N 2 -t 2:8300
+
+mkfs.fat -F32 /dev/sda1
+mkfs.ext4 /dev/sda2
+
+mount /dev/sda2 /mnt
+mkdir -p /mnt/boot
+mount /dev/sda1 /mnt/boot
+
+pacstrap /mnt base base-devel linux-lts linux-firmware neovim
