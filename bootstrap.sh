2f3da9b9ceb1f154badfcbe262dad47a1963f9e7
in-no-tech <82801408+in-no-tech@users.noreply.github.com>
2021-04-19 12:56:34 +0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 sgdisk --zap-all /dev/sda
 sgdisk -o /dev/sda
-sgdisk -n 1:0:+256M -t 1:ef00 -N 2 -t 2:8300
+sgdisk -n 1:0:+256M -t 1:ef00 -N 2 -t 2:8300 /dev/sda
 
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
